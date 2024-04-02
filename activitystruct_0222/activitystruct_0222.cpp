728b604026c9d43658b28dca4ae5f4310833b74c
IndraHfid <indrahafidsaputra@gmail.com>
2024-04-02 13:49:48 +0700

    Add project files.

---
+++ activitystruct_0222/activitystruct_0222.cpp
@@ -0,0 +1,19 @@
+// activitystruct_0222.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+struct Mahasiswa {
+    string nim;
+    string nama;
+    string alamat;
+    string umur;
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
